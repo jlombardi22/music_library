477963914869dfee091cac724bf68192e06cd455
Johnny Lombardi <jlombardi004@gmail.com>
2022-04-25 12:17:18 -0700

    create the http post to add a song to the music library app

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Songs(models.Model):
+    title = models.CharField(max_length=255, null=True)
+    artist = models.CharField(max_length=255, null=True)
+    album = models.CharField(max_length=255, null=True)
+    release_date = models.CharField(max_length=255, null=True)
+    genre = models.CharField(max_length=255, null=True)
--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from songs.serializer import SongsSerializer
+from rest_framework import status
+from songs.models import Songs
 # Create your views here.
+
+
+@api_view(['POST'])
+def songs_list(request):
+
+    if request.method == 'POST':
+        serializer = SongsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
